f70c787877c5f38eb54291822d8b42522304638b
Batchu Thrishna <batchuthrishna@gmail.com>
2019-11-28 23:06:49 +0530

    Implemented customized button

---
+++ 09ButtonCustom/app/src/main/java/com/example/buttoncustom/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.buttoncustom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        //button.setEnabled(false);
+    }
+}
